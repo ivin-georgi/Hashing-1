fa79a7ee6c0e60094658f1ee1df1f6fe7e729b45
Ivin Issac Georgi <superlonelyhero@gmail.com>
2025-08-19 11:24:09 -0500

    Done Hashing-1

---
+++ Isomorphic Strings.java
@@ -0,0 +1,25 @@
+public class Main {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> sMap = new HashMap<>();
+        HashMap<Character, Character> tMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+
+            if (sMap.containsKey(sChar)) {
+                if (sMap.get(sChar) != tChar) return false;
+            } else {
+                sMap.put(sChar, tChar);
+            }
+
+            if (tMap.containsKey(tChar)) {
+                if (tMap.get(tChar) != sChar) return false;
+            } else {
+                tMap.put(tChar, sChar);
+            }
+        }
+
+        return true;
+    }
+}
